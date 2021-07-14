187225d6e0b9ba14261d68415e478ae9bae43467
jessaclark <jemahold@icloud.com>
2021-07-14 16:11:41 -0400

    define express controller

---
+++ controllers/posts.js
@@ -0,0 +1,10 @@
+import Post from "../models/post.js";
+
+export const getProjects = async (req, res) => {
+  try {
+    const post = await Post.find();
+    res.json(post);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
