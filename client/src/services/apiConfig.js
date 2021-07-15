95bd5184a64b5a75a95d89b8ca35149383fffdfa
rahulpjo <rjoshi9000@gmail.com>
2021-07-15 09:25:34 -0700

    Create axios calls in services

---
+++ client/src/services/apiConfig.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+let apiUrl;
+
+const apiUrls = {
+  production: "https://sour-full-stack-blog.herokuapp.com/api",
+  development: "http://localhost:3000/api",
+};
+
+if (window.location.hostname === "localhost") {
+  apiUrl = apiUrls.development;
+} else {
+  apiUrl = apiUrls.production;
+}
+
+const api = axios.create({ baseURL: apiUrl });
+
+export default api;
