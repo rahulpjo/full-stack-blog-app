8d14a8e5ac5f7a818e14c97092e174c98c9cd164
rahulpjo <rjoshi9000@gmail.com>
2021-07-15 11:53:19 -0700

    Add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
+import { Route, Switch } from "react-router";
 import "./App.css";
+import Home from "./screens/Home/Home";
+import AllPosts from "./screens/AllPosts/AllPosts";
+import PostDetail from "./screens/PostDetail/PostDetail";
+import PostEdit from "./screens/PostEdit/PostEdit";
+import PostCreate from "./screens/PostCreate/PostCreate";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/posts" component={AllPosts} />
+        <Route exact path="/posts/:id" component={PostDetail} />
+        <Route exact path="/posts/:id/edit" component={PostEdit} />
+        <Route path="/new" component={PostCreate} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
