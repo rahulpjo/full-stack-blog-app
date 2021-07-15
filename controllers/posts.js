9797bdd7be4d0bfc5018aa1b007ce587707471f9
rahulpjo <rjoshi9000@gmail.com>
2021-07-15 08:29:10 -0700

    Create route and controller to delete post

--- controllers/posts.js
+++ controllers/posts.js
@@ -37,3 +37,16 @@ export const updatePost = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+export const deletePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await Post.findByIdAndDelete(id);
+    if (deleted) {
+      return res.status(200).send("Post deleted!");
+    }
+    throw new Error("Post not found!");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
--- routes/posts.js
+++ routes/posts.js
@@ -7,5 +7,6 @@ router.get("/posts", controllers.getPosts);
 router.get("/posts/:id", controllers.getSinglePost);
 router.post("/posts", controllers.createPost);
 router.put("/posts/:id", controllers.updatePost);
+router.delete("/posts/:id", controllers.deletePost);
 
 export default router;
