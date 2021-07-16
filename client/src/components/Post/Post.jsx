d583290e27e86873d8c4aa362c2a50a5acf1b0e2
rahulpjo <rjoshi9000@gmail.com>
2021-07-16 09:30:56 -0700

    Add styling to post

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -4,12 +4,16 @@ import "./Post.css";
 const Post = (props) => {
   const { _id, title, imageURL, content, author } = props.post;
   return (
-    <Link to={`/posts/${_id}`}>
-      <article className="post">
-        <h2>{title}</h2>
-        <h5>{author}</h5>
-        <img src={imageURL} alt={title} />
-        <p>{content}</p>
+    <Link className="post" to={`/posts/${_id}`}>
+      <article>
+        <section className="post-text">
+          <h2>{title}</h2>
+          <h5>{author}</h5>
+          <p>{content}</p>
+        </section>
+        <section className="post-image">
+          <img src={imageURL} alt={title} />
+        </section>
       </article>
     </Link>
   );
